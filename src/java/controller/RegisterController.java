e020ca5585081ba553bb3a920eb9b704946da563
NGUYEN MINH TUAN <nguyenminhtuan271203@gmail.com>
2023-05-31 14:04:18 +0700

    Added DB context, css, fonts, images, js, login-register.jsp, model Customer, RegisterController, web.xml

---
+++ src/java/controller/RegisterController.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import model.Customer;
+
+/**
+ *
+ * @author Acer Aspire
+ */
+public class RegisterController extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String address = req.getParameter("address");
+        String email = req.getParameter("email");
+        String pass = req.getParameter("pass");
+        String rePass = req.getParameter("rePass");
+        
+        Customer c = new Customer(name,pass,address,email);
+        
+        if(pass.equals(rePass) && req.getParameter("register")!=null){
+            c.register();
+            String fail = "Register Successfully";
+            req.setAttribute("fail", fail);
+            req.getRequestDispatcher("login-register.jsp").forward(req, resp);
+        }else{
+            String fail = "Password must be the same as Confirm Password";
+            req.setAttribute("fail", fail);
+            req.getRequestDispatcher("login-register.jsp").forward(req, resp);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+    
+}
