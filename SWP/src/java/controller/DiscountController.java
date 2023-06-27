8b0bcb15c6a83ad0f7633614e7a9174e3bfb85e2
nav0511 <133015300+nav0511@users.noreply.github.com>
2023-06-27 12:46:11 +0700

    modify order history
    modify order history

---
+++ SWP/src/java/controller/DiscountController.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ *
+ * @author admin
+ */
+public class DiscountController extends HttpServlet {
+    
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+        
+    }
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        if (session.getAttribute("admin") == null) {
+            resp.sendRedirect("Admin_login.jsp");
+        } else {
+            resp.sendRedirect("Admin_page.jsp");
+        }
+        
+    }
+    
+}
