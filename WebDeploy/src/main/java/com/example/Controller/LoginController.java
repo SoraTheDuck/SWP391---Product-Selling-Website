0c5593983bdee49aadfcde61384f24dfaad2374b
SoraTheDuck <soratheclone@gmail.com>
2023-06-29 23:02:08 +0700

    Deploy Form

---
+++ WebDeploy/src/main/java/com/example/Controller/LoginController.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.Controller;
+
+import model.Customer;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+
+@WebServlet(name = "LoginController", urlPatterns = "/login")
+
+public class LoginController extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //Nhận thông tin
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+        String r = req.getParameter("rem");
+        
+        //tao 3 cookie moi
+        Cookie cmail = new Cookie("cmail", email);
+        Cookie cpassword = new Cookie("cpassword",password);
+        Cookie cr = new Cookie("cr",r);
+        
+        if(r!=null){
+            cmail.setMaxAge(60*60*24*7);
+            cpassword.setMaxAge(60*60*24*7);
+            cr.setMaxAge(60*60*24*7);
+        }else{
+            cmail.setMaxAge(0);
+            cpassword.setMaxAge(0);
+            cr.setMaxAge(0);
+        }
+        
+        resp.addCookie(cmail);
+        resp.addCookie(cpassword);
+        resp.addCookie(cr);
+
+        //Xử lí thông tin
+        Customer c = new Customer(email, password);
+        boolean check = c.checkLogin();
+
+        //Trả kết quả về
+        if (check) {
+            //Lưu thông tin login vào session
+            HttpSession session = req.getSession();
+            c.getCustomerByAccount(email);
+            session.setAttribute("cus", c);
+            req.setAttribute("c", c.getName());
+            
+            req.getRequestDispatcher("home").forward(req, resp);     
+        }
+        else {
+            req.setAttribute("email", email);
+            req.setAttribute("password", password);
+            req.setAttribute("mess", "Account or Password is not correct !!");
+            req.getRequestDispatcher("Login.jsp").forward(req, resp);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("Login.jsp").forward(req, resp);
+    }
+    
+}
---
+++ WebDeploy/src/main/java/com/example/Controller/RegisterController.java
@@ -0,0 +1,114 @@
+package com.example.Controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.Random;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import model.Customer;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "RegisterController", urlPatterns = "/register")
+public class RegisterController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("Register.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //req.getRequestDispatcher("Register.jsp").forward(req, resp);
+
+
+        String name = req.getParameter("name");
+        String address = req.getParameter("address");
+        String email = req.getParameter("email");
+        String pass = req.getParameter("pass");
+        String rePass = req.getParameter("rePass");
+
+        Customer c = new Customer(name,pass,address,email);
+
+        int otp = 0;
+        boolean con = true;
+        HttpSession mySession = req.getSession();
+
+
+
+        if(!pass.equals(rePass)){
+            req.setAttribute("mess", "Password must be the same as Confirm Password !!");
+            req.getRequestDispatcher("Register.jsp").forward(req, resp);
+        }else{
+            boolean check;
+            check = c.checkCustomerExist(email);
+
+            if(check){
+                String fail = "Email is already registred !! Please use a different email address !!";
+                req.setAttribute("mess", fail);
+                req.getRequestDispatcher("Register.jsp").forward(req, resp);
+            }else{
+
+                //generate otp
+                Random rand = new Random();
+                otp = rand.nextInt(1255650);
+
+                String to = email;
+                //get the session object
+                Properties props = new Properties();
+                props.put("mail.smtp.host", "smtp.gmail.com");
+                props.put("mail.smtp.socketFactory.port", "465");
+                props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+                props.put("mail.smtp.auth", "true");
+                props.put("mail.smtp.port", "465");
+
+                Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication("tofunm271203@gmail.com", "vdhvnaoublgvhjcu");
+                    }
+                });
+
+                //email message content
+                try {
+                    MimeMessage message = new MimeMessage(session);
+                    message.setFrom(new InternetAddress(email));// change accordingly
+                    message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+                    message.setSubject("Headphone Website");
+                    message.setText("Your code for verification is: " + otp);
+                    // send message
+                    Transport.send(message);
+                    System.out.println("Email sent successfully");
+                }catch(MessagingException e){
+                    System.out.println("Send email failed");
+                    con = false;
+                }
+
+                if(con){
+                    req.setAttribute("message","The verification code is sent to your email id");
+                    mySession.setAttribute("otp",otp);
+                    mySession.setAttribute("customer", c);
+                    req.getRequestDispatcher("VerifyEmail.jsp").forward(req, resp);
+                }else{
+                    req.setAttribute("mess", "Something went wrong. Please try again later !!");
+                    req.getRequestDispatcher("Register.jsp").forward(req, resp);
+
+                }
+
+            }
+        }
+    }
+}
