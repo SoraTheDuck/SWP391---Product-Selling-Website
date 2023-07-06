d1ab0fbd8476933a5db8e16a0866d2abc67ae492
nav0511 <133015300+nav0511@users.noreply.github.com>
2023-07-06 23:05:48 +0700

    add staff
    add staff

---
+++ SWP/src/java/controller/DiscountController.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+import model.Admin;
+import model.Product;
+
+/**
+ *
+ * @author admin
+ */
+public class DiscountController extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("id");
+
+        Product p = new Product();
+        boolean dis = p.checkDiscount(id);
+
+        if (dis) {
+            List<Product> list = p.getAllProductReleaseDate();
+            req.setAttribute("list", list);
+            req.setAttribute("mess", "Cannot add discount!");
+            req.getRequestDispatcher("Discount.jsp").forward(req, resp);
+        } else {
+            Product pr = p.getProductByID2(id);
+            req.setAttribute("p", pr);
+            req.getRequestDispatcher("ProductDiscount.jsp").forward(req, resp);
+        }
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        Admin admin = (Admin) session.getAttribute("admin");
+        
+        if (admin != null) {
+            Product p = new Product();
+            List<Product> list = p.getAllProductReleaseDate();
+            req.setAttribute("list", list);
+            req.getRequestDispatcher("Discount.jsp").forward(req, resp);
+        } else {
+            req.getRequestDispatcher("home").forward(req, resp);
+        }
+    }
+
+}
---
+++ SWP/web/EditProduct.jsp
@@ -0,0 +1,37 @@
+<%-- 
+    Document   : EditProduct
+    Created on : Jul 4, 2023, 8:06:59 PM
+    Author     : admin
+--%>
+
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h3 style="color: red">${mess}</h3>
+        <form action="editproduct" method="post">
+            Product ID <input type="text" name="id" value="${p.getId()}" readonly>
+            <br><br>Product Name <input type="text" name="name" value="${p.getName()}">
+            <br><br>Product Price <input type="text" name="price" value="${p.getPrice()}">
+            <br><br>Image <input type="text" name="image" value="${p.getImage()}">
+            <br><br>Quantity <input type="number" name="quantity" value="${p.getQuantity()}">
+            <c:choose>
+                <c:when test="${p.getWire()=='Wired'}">
+                    <br><br>Wire <input type="checkbox" name="wire" checked>
+                </c:when>
+                <c:otherwise>
+                    <br><br>Wire <input type="checkbox" name="wire">
+                </c:otherwise>
+            </c:choose>
+            <br><br>Description <textarea name="description">${p.getDescription()}</textarea>
+            <br><br>Release Date <input type="text" name="releasedate" value="${p.getReleasedate()}" readonly>
+            <br><br>Changelog <input type="text" name="change" value="${log}">
+            <br><br><input type="submit" value="Edit">
+        </form>
+    </body>
+</html>
