fcb85d5aa480c88b62f88c2e96f14ef07804bb78
nav0511 <133015300+nav0511@users.noreply.github.com>
2023-07-06 22:03:58 +0700

    edit
    edit

---
+++ SWP/src/java/controller/StaffManageController.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import model.Staff;
+
+/**
+ *
+ * @author admin
+ */
+public class StaffManageController extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Staff s=new Staff();
+        List<Staff> list=s.getListStaff();
+        req.setAttribute("list", list);
+        req.getRequestDispatcher("StaffList.jsp").forward(req, resp);
+    }
+    
+}
