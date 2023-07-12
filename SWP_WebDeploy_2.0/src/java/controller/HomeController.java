56274823d14310f47e73342cc960f06040709132
nav0511 <133015300+nav0511@users.noreply.github.com>
2023-07-12 15:54:04 +0700

    without reject, w/ small update

---
+++ SWP_WebDeploy_2.0/src/java/controller/HomeController.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import model.Category;
+import model.Product;
+
+/**
+ *
+ * @author Acer Aspire
+ */
+public class HomeController extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Product p = new Product();
+        Category c = new Category();
+        List<Category> list = c.getAllCategory();
+        List<Product> hlist = p.getAllProductsByCategory2(1);
+        List<Product> elist = p.getAllProductsByCategory2(2);
+        List<Product> hslist = p.getAllProductsByCategory2(3);
+        List<Product> alist = p.getAllProductsByCategory2(4);
+        req.setAttribute("categoryList", list);
+        req.setAttribute("earphoneList", elist);
+        req.setAttribute("headphoneList", hlist);
+        req.setAttribute("headsetList", hslist);
+        req.setAttribute("airpodList", alist);
+        req.getRequestDispatcher("Home.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Product p = new Product();
+        Category c = new Category();
+        List<Category> list = c.getAllCategory();
+        List<Product> hlist = p.getAllProductsByCategory2(1);
+        List<Product> elist = p.getAllProductsByCategory2(2);
+        List<Product> hslist = p.getAllProductsByCategory2(3);
+        List<Product> alist = p.getAllProductsByCategory2(4);
+        List<Product> tlist = p.getTrendingProduct();
+        req.setAttribute("categoryList", list);
+        req.setAttribute("earphoneList", elist);
+        req.setAttribute("headphoneList", hlist);
+        req.setAttribute("headsetList", hslist);
+        req.setAttribute("airpodList", alist);
+        req.setAttribute("trendingList", tlist);
+        req.getRequestDispatcher("Home.jsp").forward(req, resp);
+    }
+    
+}
