c1f2600c1c5b00f918d788973500e5058fb395b9
nav0511 <133015300+nav0511@users.noreply.github.com>
2023-07-13 12:43:03 +0700

    order manager
    order manager

---
+++ SWP_WebDeploy_2.0/src/java/controller/StaffLoginController.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+import model.Order;
+import model.Staff;
+
+/**
+ *
+ * @author admin
+ */
+public class StaffLoginController extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //Nhận thông tin
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+
+        //Xử lí thông tin
+        Staff s = new Staff(email, password);
+        boolean check = s.checkLogin();
+
+        //Trả kết quả về
+        if (check) {
+            //Lưu thông tin login vào session
+            String role = s.getRole();
+            Staff s1 = s.getStaff();
+            HttpSession session = req.getSession();
+            
+            if (role.equals("Accountant")) {
+                session.setAttribute("staff", s1);
+                session.setAttribute("accountant", "accountant");
+                req.getRequestDispatcher("Accountant.jsp").forward(req, resp);
+            } else if (role.equals("Marketing")) {
+                session.setAttribute("staff", s1);
+                session.setAttribute("marketing", "marketing");
+                req.getRequestDispatcher("Marketing.jsp").forward(req, resp);
+            } else {
+                Order o=new Order();
+                List<Order> list=o.getListNotCompletedOrder(s1.getId());
+                req.setAttribute("ord_id", s1.getId());
+                req.setAttribute("list", list);
+                req.getRequestDispatcher("OrderManager.jsp").forward(req, resp);
+            }
+
+        } else {
+            req.setAttribute("email", email);
+            req.setAttribute("password", password);
+            req.setAttribute("mess", "Tài khoản hoặc mật khẩu không đúng!");
+            req.getRequestDispatcher("Staff_login.jsp").forward(req, resp);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("Staff_login.jsp").forward(req, resp);
+    }
+
+}
---
+++ SWP_WebDeploy_2.0/src/java/controller/admin/ListProductController.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller.admin;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.List;
+import model.Product;
+
+/**
+ *
+ * @author Admin
+ */
+public class ListProductController extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Product product = new Product();
+        List<Product> list = product.getAllProduct();
+        
+        req.setAttribute("listPro", list);
+        req.getRequestDispatcher("Admin_products.jsp").forward(req, resp);
+    }
+
+}
